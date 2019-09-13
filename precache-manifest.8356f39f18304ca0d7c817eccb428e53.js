fe8dec19fbfcb7e22a3375ddb01123f17e0b9b3e
Senthilkannan <senthilkannanarmy@gmail.com>
2019-09-13 03:06:55 -0700

    Updates

---
+++ precache-manifest.8356f39f18304ca0d7c817eccb428e53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea232776ec9a9afa038e102efd43c74",
+    "url": "/clicker/index.html"
+  },
+  {
+    "revision": "4d4f221b7cf26ec832f4",
+    "url": "/clicker/static/css/main.4ed4f653.chunk.css"
+  },
+  {
+    "revision": "208346bb541eadf5fbd1",
+    "url": "/clicker/static/js/2.5276de9b.chunk.js"
+  },
+  {
+    "revision": "4d4f221b7cf26ec832f4",
+    "url": "/clicker/static/js/main.c0cf9067.chunk.js"
+  },
+  {
+    "revision": "3a4ec7b01d3b119a8ca3",
+    "url": "/clicker/static/js/runtime~main.4924eb59.js"
+  }
+]);
